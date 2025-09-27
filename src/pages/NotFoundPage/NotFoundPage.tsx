8e2a445cea4f640dd5fe49343197c06e891c3628
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-09-27 13:01:26 +0300

    feat: add not found page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,9 +1,16 @@
+import s from './NotFoundPage.module.css';
 import Container from '../../components/common/Container/Container';
+import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => {
   return (
-    <section>
-      <Container>Not Found Page</Container>
+    <section className={s.notFoundPageSection}>
+      <Container>
+        <h2 className={s.notFoundPageTitle}>Page not found</h2>
+        <Link to='/' className={s.notFoundPageLink}>
+          Go back to home page
+        </Link>
+      </Container>
     </section>
   );
 };
